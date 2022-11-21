281bb0ae25b005cc89cf906b601db0063538e67d
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2022-11-21 19:45:13 +0100

    Fix image loading and cli parameters

--- main.py
+++ main.py
@@ -9,19 +9,26 @@ def image_difference(img_a, img_b):
 
 # Cli parameters
 
-if len(sys.argv) != 2:
-    sys.exit("Usage:\npython main.py target_img")
+if len(sys.argv) < 2:
+    sys.exit("Usage:\npython main.py target_img <canvas_img>")
 
 target_img_dir = sys.argv[1]
-canvas_img_dir = sys.argv[2] if len(sys.argv >= 2) else None
+canvas_img_dir = sys.argv[2] if len(sys.argv) > 2 else None
 
-# GA parameters
+# Load images
 
 target_img = cv.imread("assets/" + target_img_dir)
 
-canvas_img_dir = cv.imread("assets/" + canvas_img_dir) if canvas_img_dir else 
-if canvas_img_dir != None:
-    canvas_img = cv.imread("assets/" + canvas_img_dir)
+canvas_img = cv.imread("assets/" + canvas_img_dir) if canvas_img_dir else np.full_like(target_img, (255, 255, 255))
+if (canvas_img.shape != target_img).all():
+    sys.exit("Canvas image and target image must be the same size!")
+
+
+
+
+
+
+# GA parameters
 
 fitness_function = lambda gene, gene_idx : 1.0 / image_difference(target_img, image_from_gene(canvas_img, gene))
 
@@ -40,3 +47,12 @@ def on_stop(ga_instance, last_population_fitness):
 #ga_instance = pygad.GA(num_generations=num_generations...
 
 
+
+
+
+
+
+# Tests
+
+cv.imshow("XD", canvas_img)
+cv.waitKey()
