cadd1959577f3383fafe784b6472385ecb44c0ec
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-01-03 23:37:29 +0100

    Fix OpenCV output window "Stopped responding" error by adding a dedicated display process

--- src/main.py
+++ src/main.py
@@ -1,14 +1,39 @@
 import cli
 import cv2 as cv
 from genetic_artist import GeneticArtist
+from multiprocessing import Process, Queue
 
-genetic_artist = GeneticArtist(cli.ARGS.TARGET_IMG_FILE, cli.ARGS.STROKE_IMG_DIR,
-                               canvas_img_path=cli.ARGS.CANVAS_IMG_FILE,
-                               config_file_path=cli.ARGS.GENETIC_ARTIST_CONFIG_FILE)
 
-for _ in range(256):
-    genetic_artist.draw_stroke()
-    cv.imshow('Output', genetic_artist.get_image())
-    cv.waitKey(1)
+def show_progress(image_queue: Queue):
+    while True:
+        if not image_queue.empty():
+            image = image_queue.get()
+            if image is None:
+                break
+            cv.imshow('Output', image)
+        cv.waitKey(10)
+    cv.waitKey()
 
-cv.waitKey()
+
+if __name__ == '__main__':
+    # Create genetic artist
+    genetic_artist = GeneticArtist(cli.ARGS.TARGET_IMG_FILE, cli.ARGS.STROKE_IMG_DIR,
+                                   canvas_img_path=cli.ARGS.CANVAS_IMG_FILE,
+                                   config_file_path=cli.ARGS.GENETIC_ARTIST_CONFIG_FILE)
+
+    # Create image queue and display process
+    image_queue = Queue()
+    display_process = Process(target=show_progress, args=(image_queue,))
+
+    # Add blank canvas to image queue and start display process
+    image_queue.put(genetic_artist.get_image())
+    display_process.start()
+
+    # Run genetic artist for n strokes
+    for _ in range(32):
+        genetic_artist.draw_stroke()
+        image_queue.put(genetic_artist.get_image())
+
+    # End display process
+    image_queue.put(None)
+    display_process.join()
