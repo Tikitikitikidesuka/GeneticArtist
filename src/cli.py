5411769285cd724bc0369c2fb64df306b88842d0
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-01-06 17:07:44 +0100

    Update output file path checking

--- src/cli.py
+++ src/cli.py
@@ -15,9 +15,17 @@ def is_valid_file(arg: str) -> str:
     return arg
 
 
-def is_valid_filename_pv(arg: str) -> str:
-    if not is_valid_filename(arg):
-        raise argparse.ArgumentTypeError("%s is not a valid filename" % arg)
+def is_valid_new_filepath(arg: str) -> str:
+    try:
+        head, tail = os.path.split(arg)
+        if not os.path.exists(head):
+            raise argparse.ArgumentTypeError("Directory %s does not exist" % head)
+        if not is_valid_filename(tail):
+            raise argparse.ArgumentTypeError("%s is not a valid filename" % tail)
+    except ValueError:
+        # If the filepath is just a filename and not a full path,
+        # head will be an empty string and tail will be the filename
+        pass
 
     return arg
 
@@ -49,7 +57,7 @@ _parser.add_argument('-t', '--target', dest='TARGET_IMG_FILE', required=True, he
 _parser.add_argument('-s', '--strokes', dest='STROKE_IMG_DIR', required=True, help='Stroke directory path', type=is_valid_directory)
 _parser.add_argument('-c', '--config', dest='CONFIG_FILE', help='Config file path', type=is_valid_file)
 _parser.add_argument('-i', '--iterations', dest='ITERATIONS', required=True, help='Number of strokes to draw', type=is_positive_non_zero_int)
-_parser.add_argument('-o', '--output', dest='OUTPUT_FILE', required=True, help='Output image filename', type=is_valid_filename_pv)
+_parser.add_argument('-o', '--output', dest='OUTPUT_FILE', required=True, help='Output image filename', type=is_valid_new_filepath)
 _parser.add_argument('--canvas', dest='CANVAS_IMG_FILE', help='Canvas image the artist starts with', type=is_valid_file)
 _parser.add_argument('-v', '--verbose', dest='VERBOSE', help='Print the proccess\'s progress', action='store_true')
 _parser.add_argument('--nogui', dest='NO_GUI', help='Do not show the graphical interface', action='store_true')
